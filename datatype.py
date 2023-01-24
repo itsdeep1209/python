a640e6698efa13b9451463b71478a0c33b0d63be
itsdeep1209 <itsdeep1209@outlook.com>
2023-01-24 19:25:03 +0530

    update

---
+++ datatype.py
@@ -0,0 +1,11 @@
+#1.bool
+isMarried = True
+print("isMarried", isMarried , type(isMarried))
+
+#2. int
+count = 5
+print("count" , count , type (count))
+
+#3 float
+pi = 3.1415
+print("pi" , pi , type(pi))
