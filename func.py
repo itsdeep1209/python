68ef2e0c926965b8bb8e7bdf3bcd3ffba7475e51
itsdeep1209 <itsdeep1209@outlook.com>
2023-01-26 17:40:14 +0530

    Update func.py

--- func.py
+++ func.py
@@ -15,3 +15,20 @@ def dragons_ahead(my_list=[]):
     my_list.append(5)
     print(my_list)
 dragons_ahead()
+
+
+
+def sum_all(message1, message2, *args):
+    total = 0
+    for value in var_args:
+        total += value
+    return total
+
+result = sum_all("my list", "new list" ,1 ,2 , 3, 4 ,5, 6, 7,8 )
+print(result)
+
+def var_args(*args):
+    print(args)
+
+var_args(1, "Guido" , 66.3)
+
