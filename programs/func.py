4e76f32b3a46ff6182ef274d7a542647a8d863c7
itsdeep1209 <itsdeep1209@outlook.com>
2023-04-30 01:19:35 +0530

    update

--- programs/func.py
+++ programs/func.py
@@ -8,3 +8,19 @@ def sum(a,b):
     return a + b
 sumvalue = sum(4,5)
 print("sum is", sumvalue)
+
+# Lamnbda function : In python the functions which doesnt have a defined name is called anonymous function, In python anonymous function are defined using lambda keyword
+# and a lambda function can take any number of arguments but can only have one expression.
+
+sum = lambda a,b:a + b
+print(sum(12,13))
+
+# a function can call itself is called recursive function.
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return(x * factorial(x-1))
+num=4
+factorial_num = factorial(num)
+print("Factorial of num", num , "is", factorial_num)
