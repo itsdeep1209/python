5f226108825f33019984774e918d259e913631fa
itsdeep1209 <itsdeep1209@outlook.com>
2023-07-11 06:43:04 +0530

    Update day1.py

--- 365days/day1.py
+++ 365days/day1.py
@@ -2,3 +2,9 @@
 
 print("Python is easy \
       for everyone")
+simplelist = ["h","b","c",1,3]
+print(simplelist[2])
+simplelist.insert(5,"ab")
+print(simplelist[0:])
+print(simplelist[-1:])
+print(simplelist[::-1])
